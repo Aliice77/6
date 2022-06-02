a2a6f3643db04ba0f2b6e91c62df88f6854b8839
Aliice77 <69522330+Aliice77@users.noreply.github.com>
2022-06-03 00:08:20 +0430

    Add files via upload

---
+++ tamrin1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace tamrin1
+{
+    public class class1{
+        private string _name;
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        private int _haghdariaft;
+        public int haghdariaft
+        {
+            get { return _haghdariaft; }
+            set { _haghdariaft = value; }
+        }
+        private int _saat;
+        public int saat
+        {
+            get { return _saat; }
+            set { _saat = value; }
+        }
+
+
+        public void data(string a,int b,int c)
+        {
+            Console.WriteLine("Name: "+a);
+            Console.WriteLine("haghzahmeh: "+b);
+            Console.WriteLine("saatKarDarroz : "+c);
+            Console.WriteLine("daramad mahiuane: "+b*c*24);
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            class1 cl=new class1();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter your name: ");
+            cl.name=Console.ReadLine();
+            Console.Write("Enter your haghzahmat: ");
+            cl.haghdariaft= Convert.ToInt32( Console.ReadLine());
+            Console.Write("Enter your hour: ");
+            cl.saat=Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine();
+            cl.data(cl.name, cl.haghdariaft, cl.saat);
+            
+            }
+
+        }
+    }
+}
